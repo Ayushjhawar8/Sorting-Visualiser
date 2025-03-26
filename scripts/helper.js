e50f04f4d890f6959008e997ee2816633182a3c0
Aditya <adityasahoo652@gmail.com>
2025-03-26 17:07:46 +0530

    Improve height calculation for visual elements to enhance visibility

--- scripts/helper.js
+++ scripts/helper.js
@@ -40,10 +40,16 @@ class Helper {
         await this.pause();
         let value1 = this.list[index1].getAttribute("value");
         let value2 = this.list[index2].getAttribute("value");
+
+        const baseHeight = 15;  // Minimum height for visibility
+        const scalingFactor = 3.8;  // Scaling for larger values
+
         this.list[index1].setAttribute("value", value2);
-        this.list[index1].style.height = `${3.8 * value2}px`;
+        this.list[index1].style.height = `${baseHeight + scalingFactor * value2}px`;
+
         this.list[index2].setAttribute("value", value1);
-        this.list[index2].style.height = `${3.8 * value1}px`;
+        this.list[index2].style.height = `${baseHeight + scalingFactor * value1}px`;
+
         let span1 = this.list[index1].querySelector(".cell-value");
         let span2 = this.list[index2].querySelector(".cell-value");
 
